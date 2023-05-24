4ee048abafcc045b21fe9b92102ca2ff56abc302
BR0M1CH <322panda322panda322@gmail.com>
2023-05-24 20:13:44 +0300

    Success st1

--- RBTree.py
+++ RBTree.py
@@ -3,91 +3,103 @@ import random
 
 class Node:
 
-    
-    def __init__(self, data, parent):
-        self.data = data
+    def __init__(self, data):
         self.left = self.right = None
-        self.parent = parent
+        self.data = data
 
-    
+    def addleft(self, data):
+        self.left = Node(data)
+
+    def addright(self, data):
+        self.right = Node(data)
+
     def hasleft(self):
-        return(self.left)
-    
+        if self.left != None:
+            return True
+        return False
     
     def hasright(self):
-        return(self.right)
+        if self.right != None:
+            return True
+        return False
+    
 
 class Tree:
 
-    currentNode = None
-
-    
-    def __init__(self, data):
-        self.root = Node(data, None)
+    def __init__(self, data):   
+        self.root = Node(data)
+        self.size = 1
         self.currentNode = self.root
 
-    
-    def put(self, data):
-            
-            if data < self.currentNode.data:
-                if self.currentNode.hasleft():
-                    self.currentNode = self.currentNode.left
-                    self.put(data)
-                else:
-                    self.currentNode.left = Node(data, self.currentNode)
-                    self.currentNode = self.root
-                    return
-                      
-            elif data > self.currentNode.data:
-                if self.currentNode.hasright():
-                    self.currentNode = self.currentNode.right
-                    self.put(data)
-                else:
-                    self.currentNode.right = Node(data, self.currentNode)
-                    self.currentNode = self.root
-                    return
-            
-            elif data == self.currentNode.data:
-                self.currentNode = self.root
-                print("This num already in tree")
-                return
-
-    
-    def find(self, data):
-
-
+    def add(self, data):
+        
         if data < self.currentNode.data:
             if self.currentNode.hasleft():
                 self.currentNode = self.currentNode.left
+                self.add(data)
             else:
+                self.currentNode.addleft(data)
+                self.size += 1
                 self.currentNode = self.root
-                return False
         
         elif data > self.currentNode.data:
             if self.currentNode.hasright():
                 self.currentNode = self.currentNode.right
+                self.add(data)
+            else:
+                self.currentNode.addright(data)
+                self.size += 1
+                self.currentNode = self.root
+
+    
+    def find(self, data):
+        if data < self.currentNode.data:
+            if self.currentNode.hasleft():
+                self.currentNode = self.currentNode.left
+                self.find(self, data)
             else:
                 self.currentNode = self.root
                 return False
-                
-
+            
+        elif data > self.currentNode.data:
+            if self.currentNode.hasright():
+                self.currentNode = self.currentNode.right
+                self.find(self, data)
+            else:
+                self.currentNode = self.root
+                return False
+        
         elif data == self.currentNode.data:
             self.currentNode = self.root
             return True
-        
 
-    def treeprint(self):
-        pass
-
-        
-            
-        
-
-if __name__ == "__main__":
     
-    rbtree = Tree(50)
-    for i in range(20):
-        a = random.randint(0,100)
-        print(a)
-        rbtree.put(a)
-    rbtree.treeprint()
+    def show(self, node):
+        print(node.data)
+        if node.hasleft():
+            self.show(node.left)
+        if node.hasright():
+            self.show(node.right)
+
+
+
+t = Tree(10)
+t.add(5)
+print("5 added")
+t.add(2)
+print("2 added")
+t.add(4)
+print("4 added")
+t.add(3)
+print("3 added")
+t.add(15)
+print("15 added")
+t.add(16)
+print("16 added")
+t.add(19)
+print("19 added")
+t.add(12)
+print("12 added")
+t.add(10)
+print("10 added")
+t.show(t.root)
